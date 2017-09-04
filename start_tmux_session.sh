1ee690b950fe6a3a0bdaebd059f1a6b26c13865a
Vojta Jina <vojta@apple.com>
2017-09-04 07:54:12 -0700

    Fix paths in start_tmux_session.sh

--- start_tmux_session.sh
+++ start_tmux_session.sh
@@ -2,9 +2,9 @@
 
 tmux new-session -s fastai -n main -d
 tmux new-window -t fastai:1 -n jupyter
-tmux send-keys -t fastai:1 "cd nbs" C-m
+tmux send-keys -t fastai:1 "cd ~/courses/deeplearning1/nbs" C-m
 tmux send-keys -t fastai:1 "jupyter notebook" C-m
 tmux select-window -t fastai:0
-tmux send-keys -t fastai:0 "cd courses/deeplearning1/nbs" C-m
+tmux send-keys -t fastai:0 "cd ~/courses/deeplearning1/nbs" C-m
 tmux split-window -h
-tmux send-keys -t fastai:0 "cd courses/deeplearning1/nbs" C-m
+tmux send-keys -t fastai:0 "cd ~/courses/deeplearning1/nbs" C-m
